37b8c1025cb83b2167906787eb75eb3a9ca5d4b4
IvKorolev <mrvanivan@mail.ru>
2025-03-27 19:24:43 +0300

    Some changes

--- lab1/task7/main.c
+++ lab1/task7/main.c
@@ -18,13 +18,11 @@ void list_files(const char *path) {
 
     printf("%s:\n", path);
 
-    char full_path[MAX_PATH];
     while ((entry = readdir(dir)) != NULL) {
-
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
             continue;
         }
-
+        char full_path[MAX_PATH];
         snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
 
         if (stat(full_path, &file_stat) == -1) {
@@ -32,7 +30,10 @@ void list_files(const char *path) {
             continue;
         }
 
-        printf("%-20s %lu\n", entry->d_name, entry->d_ino);
+        if (S_ISREG(file_stat.st_mode)) {
+            printf("%-20s %lu\n", entry->d_name, file_stat.st_blocks);
+        }
+        //printf("%-20s %lu\n", entry->d_name, entry->d_ino);
     }
 
     closedir(dir);
