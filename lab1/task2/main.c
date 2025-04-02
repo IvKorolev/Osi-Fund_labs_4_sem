0194b01e539d5a17b81737971618df2da9ceeecc
IvKorolev <mrvanivan@mail.ru>
2025-04-02 23:17:05 +0300

    Seems to be done

--- lab1/task2/main.c
+++ lab1/task2/main.c
@@ -22,7 +22,8 @@ enum errors {
     WRONG_PIN,
 };
 
-void print_error(enum errors err) {
+void print_error(enum errors err)
+{
     switch (err) {
     case INVALID_INPUT:
         printf("Ошибка: Некорректный ввод.\n");
@@ -44,27 +45,49 @@ void print_error(enum errors err) {
     }
 }
 
-enum errors str_to_int(const char* x, int* res){
+enum errors str_to_int(const char* x, int* res)
+{
     char symbol;
-    if (sscanf(x, "%d%c", res, &symbol) != 1){
+    if (sscanf(x, "%d%c", res, &symbol) != 1)
+    {
         return INVALID_INPUT;
     }
 
-    if (*res >= INT_MAX || *res <= INT_MIN){
+    if (*res >= INT_MAX || *res <= INT_MIN)
+    {
         return INVALID_INPUT;
     }
 
     return OK;
 }
 
-enum errors xor2(FILE *file) {
-    if (file == NULL) {
+enum errors str_to_uint(const char *str, unsigned int *out, int base)
+{
+    if (str == NULL || *str == '\0' || out == NULL || base < 2 || base > 36)
+    {
+        return INVALID_INPUT;
+    }
+    char *end_ptr;
+    unsigned long value = strtoul(str, &end_ptr, base);
+    if (end_ptr == str || *end_ptr != '\0' || value > UINT_MAX)
+    {
+        return INVALID_INPUT;
+    }
+    *out = (unsigned int) value;
+    return OK;
+}
+
+enum errors xor2(FILE *file)
+{
+    if (file == NULL)
+    {
         return INVALID_INPUT;
     }
     char one;
     char result = 0x00;
     one = 0x00;
-    while (fread(&one, 1, 1, file)) {
+    while (fread(&one, 1, 1, file))
+    {
         char first_4_bite = one >> 4;
 
         char last_4_bite = one & 0x0F;
@@ -75,24 +98,30 @@ enum errors xor2(FILE *file) {
     return OK;
 }
 
-enum errors xor3(FILE *file) {
-    if (file == NULL) {
+enum errors xor3(FILE *file)
+{
+    if (file == NULL)
+    {
         return INVALID_INPUT;
     }
     char result = 0x00;
     char one = 0x00;
-    while (fread(&one, 1, 1, file)) {
+    while (fread(&one, 1, 1, file))
+    {
         result ^= one;
     }
     printf("xor3 result is: %x\n", result);
     return OK;
 }
 
-enum errors xor4(FILE *file) {
-    if (file == NULL) {
+enum errors xor4(FILE *file)
+{
+    if (file == NULL)
+    {
         return INVALID_INPUT;
     }
-    union {
+    union
+    {
         char bytes[2];
         uint16_t num;
     } result;
@@ -100,89 +129,113 @@ enum errors xor4(FILE *file) {
     result.num = 0;
     char bytes_buffer[2] = {0x00, 0x00};
     int bytes_read = 0;
-    while ((bytes_read = fread(&bytes_buffer, sizeof(bytes_buffer), 1, file)) != 0) {
-        if (bytes_read < sizeof(bytes_buffer)) {
-            for (size_t i = bytes_read; i < sizeof(bytes_buffer); i++) {
+    while ((bytes_read = fread(&bytes_buffer, sizeof(bytes_buffer), 1, file)) != 0)
+    {
+        if (bytes_read < sizeof(bytes_buffer))
+        {
+            for (size_t i = bytes_read; i < sizeof(bytes_buffer); i++)
+            {
                 bytes_buffer[i] = 0x00;
             }
         }
-        for (int i = 0; i < sizeof(bytes_buffer); i++) {
+        for (int i = 0; i < sizeof(bytes_buffer); i++)
+        {
             result.bytes[i] ^= bytes_buffer[i];
         }
         memset(bytes_buffer, 0x00, sizeof(bytes_buffer));
     }
     printf("xor4 result is: ");
-    for (int i = 0; i < sizeof(bytes_buffer); i++) {
+    for (int i = 0; i < sizeof(bytes_buffer); i++)
+    {
         printf("%x ", result.bytes[i]);
     }
     printf("\nNUMBER IS  %d(10) %x(hex)\n", result.num, result.num);
     return OK;
 }
 
-enum errors xor5(FILE *file) {
-    if (file == NULL) {
+enum errors xor5(FILE *file)
+{
+    if (file == NULL)
+    {
         return INVALID_INPUT;
     }
-    union {
+    union
+    {
         char bytes[4];
         uint32_t num;
     } result;
     result.num = 0;
     char bytes_buffer[4] = {0x00, 0x00, 0x00, 0x00};
     int bytes_read = 0;
-    while ((bytes_read = fread(&bytes_buffer, sizeof(bytes_buffer), 1, file))) {
-        if (bytes_read < sizeof(bytes_buffer)) {
-            for (int i = bytes_read; i < sizeof(bytes_buffer); i++) {
+    while ((bytes_read = fread(&bytes_buffer, sizeof(bytes_buffer), 1, file)))
+    {
+        if (bytes_read < sizeof(bytes_buffer))
+        {
+            for (int i = bytes_read; i < sizeof(bytes_buffer); i++)
+            {
                 bytes_buffer[i] = 0x00;
             }
         }
-        for (int i = 0; i < sizeof(bytes_buffer); i++) {
+        for (int i = 0; i < sizeof(bytes_buffer); i++)
+        {
             result.bytes[i] ^= bytes_buffer[i];
         }
     }
     printf("xor5 result is: ");
-    for (int i = 0; i < sizeof(bytes_buffer); i++) {
+    for (int i = 0; i < sizeof(bytes_buffer); i++)
+    {
         printf("%x ", result.bytes[i]);
     }
     printf("\nNUMBER IS  %d(10) %x(hex)\n", result.num, result.num);
     return OK;
 }
 
-enum errors xor6(FILE *file) {
-    if (file == NULL) {
+enum errors xor6(FILE *file)
+{
+    if (file == NULL)
+    {
         return INVALID_INPUT;
     }
-    union {
+    union
+    {
         char bytes[8];
         uint64_t num;
     } result;
     result.num = 0;
     char bytes_buffer[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     int bytes_read = 0;
-    while ((bytes_read = fread(&bytes_buffer, sizeof(bytes_buffer), 1, file))) {
-        if (bytes_read < sizeof(bytes_buffer)) {
-            for (int i = bytes_read; i < sizeof(bytes_buffer); i++) {
+    while ((bytes_read = fread(&bytes_buffer, sizeof(bytes_buffer), 1, file)))
+    {
+        if (bytes_read < sizeof(bytes_buffer))
+        {
+            for (int i = bytes_read; i < sizeof(bytes_buffer); i++)
+            {
                 bytes_buffer[i] = 0x00;
             }
         }
-        for (int i = 0; i < sizeof(bytes_buffer); i++) {
+        for (int i = 0; i < sizeof(bytes_buffer); i++)
+        {
             result.bytes[i] ^= bytes_buffer[i];
         }
     }
     printf("xor6 result is: ");
-    for (int i = 0; i < sizeof(bytes_buffer); i++) {
+    for (int i = 0; i < sizeof(bytes_buffer); i++)
+    {
         printf("%x ", result.bytes[i]);
     }
-    printf("\nNUMBER IS  %ld(10) %llx(hex)\n", result.num, result.num);
+    printf("\nNUMBER IS  %ld(10) %lx(hex)\n", result.num, result.num);
     return OK;
 }
 
-enum errors xor_action(char **file_paths, const int file_count, const int N) {
-    if (file_paths == NULL || N < 2 || N > 6) {
+enum errors xor_action(char **file_paths, const int file_count, const int N)
+{
+    if (file_paths == NULL || N < 2 || N > 6)
+    {
         return INVALID_INPUT;
     }
 
-    for (int i = 0; i < file_count - 1; i++) {
+    for (int i = 0; i < file_count - 1; i++)
+    {
         FILE *file = fopen(file_paths[i], "rb");
         if (file == NULL) {
             return INVALID_INPUT;
@@ -190,17 +243,28 @@ enum errors xor_action(char **file_paths, const int file_count, const int N) {
 
         printf("Processing file: %s: \n", file_paths[i]);
 
-        if (N == 2) {
+        if (N == 2)
+        {
             xor2(file);
-        } else if (N == 3) {
+        }
+        else if (N == 3)
+        {
             xor3(file);
-        } else if (N == 4) {
+        }
+        else if (N == 4)
+        {
             xor4(file);
-        } else if (N == 5) {
+        }
+        else if (N == 5)
+        {
             xor5(file);
-        } else if (N == 6) {
+        }
+        else if (N == 6)
+        {
             xor6(file);
-        } else {
+        }
+        else
+        {
             printf("N must be in {2,3,4,5,6}\n");
             fclose(file);
             return INVALID_INPUT;
@@ -212,20 +276,85 @@ enum errors xor_action(char **file_paths, const int file_count, const int N) {
     return OK;
 }
 
-enum errors copy_file(const char *src, const char *dest) {
+uint32_t swap_bytes(uint32_t value)
+{
+    // big to lit endian
+    return ((value & 0xFF000000) >> 24) |
+           ((value & 0x00FF0000) >> 8) |
+           ((value & 0x0000FF00) << 8) |
+           ((value & 0x000000FF) << 24);
+}
+
+enum errors mask_action(char **files, int size, const char *attr)
+{
+    if (files == NULL || attr == NULL)
+    {
+        return INVALID_INPUT;
+    }
+
+    unsigned int mask;
+    if (str_to_uint(attr, &mask, 16) != OK)
+    {
+        return INVALID_INPUT;
+    }
+
+    for (int i = 0; i < size - 1; i++) {
+        FILE *file = fopen(files[i], "rb");
+        if (file == NULL)
+        {
+            printf("Ошибка открытия файла: %s\n", files[i]);
+            return INVALID_MEMORY;
+        }
+
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        if (file_size % sizeof(uint32_t) != 0)
+        {
+            printf("File size is not a multiple of 4 bytes\n");
+            fclose(file);
+            return INVALID_INPUT;
+        }
+
+        int count = 0;
+
+        uint32_t number;
+        while (fread(&number, sizeof(uint32_t), 1, file) == 1)
+        {
+            number = swap_bytes(number);
+            printf("Read number: 0x%x\n", number);
+            if ((number & mask) == mask)
+            {
+                count++;
+            }
+        }
+
+        fclose(file);
+        printf("Числа, соответствующие маске %s (0x%x): %d\n", attr, mask, count);
+    }
+
+    return OK;
+}
+
+enum errors copy_file(const char *src, const char *dest)
+{
     int src_fd = open(src, O_RDONLY);
-    if (src_fd < 0) {
+    if (src_fd < 0)
+    {
         return INVALID_INPUT;
     }
 
     int dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (dest_fd < 0) {
+    if (dest_fd < 0)
+    {
         return INVALID_MEMORY;
     }
 
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
-    while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
+    while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+    {
         write(dest_fd, buffer, bytes_read);
     }
 
@@ -234,16 +363,20 @@ enum errors copy_file(const char *src, const char *dest) {
     return OK;
 }
 
-int find_string_in_file(const char *filename, const char *search_str) {
+int find_string_in_file(const char *filename, const char *search_str)
+{
     FILE *file = fopen(filename, "r");
-    if (!file) {
+    if (!file)
+    {
         printf("Ошибка открытия файла %s\n", filename);
         return 0;
     }
 
     char line[BUFFER_SIZE];
-    while (fgets(line, sizeof(line), file)) {
-        if (strstr(line, search_str)) {
+    while (fgets(line, sizeof(line), file))
+    {
+        if (strstr(line, search_str))
+        {
             fclose(file);
             return 1;
         }
@@ -251,15 +384,6 @@ int find_string_in_file(const char *filename, const char *search_str) {
 
     fclose(file);
     return 0;
-}
-
-void trim_whitespace(char *str) {
-    // Удаляем пробелы, табы, \r, \n в конце строки
-    char *end = str + strlen(str) - 1;
-    while (end >= str && (*end == '\r' || *end == '\n' || *end == ' ' || *end == '\t')) {
-        *end = '\0';
-        end--;
-    }
 }
 
 int main(int argc, char* argv[]){
@@ -275,37 +399,10 @@ int main(int argc, char* argv[]){
         const int N = argv[argc - 1][3] - '0';
         printf("N = %d\n", N);
         return xor_action(argv + 1, argc - 1, N);
-        /*enum errors result;
-        switch (argv[argc - 1][3])
-        {
-            case '2':
-                result = process_xor2(argv[argc - 1]);
-                if (result != OK) printf("Ошибка сложения по модулю\n");
-                break;
-            case '3':
-                result = process_xor3(argv[argc - 1]);
-                if (result != OK) printf("Ошибка сложения по модулю\n");
-                break;
-            case '4':
-                result = process_xor4(argv[argc - 1]);
-                if (result != OK) printf("Ошибка сложения по модулю\n");
-                break;
-            case '5':
-                result = process_xor5(argv[argc - 1]);
-                if (result != OK) printf("Ошибка сложения по модулю\n");
-                break;
-            case '6':
-                result = process_xor6(argv[argc - 1]);
-                if (result != OK) printf("Ошибка сложения по модулю\n");
-                break;
-            default:
-                printf("xor вводить от 2 до 6!\n");
-                break;
-        }*/
     }
     else if (strcmp(argv[argc - 2], "mask") == 0)
     {
-
+        return mask_action(argv + 1, argc - 2, argv[argc - 1]);
     }
     else if (strncmp(argv[argc - 1], "copy", 4) == 0)
     {
@@ -318,10 +415,12 @@ int main(int argc, char* argv[]){
             return INVALID_INPUT;
         }
 
-        for (int i = 1; i < argc - 1; i++) {
+        for (int i = 1; i < argc - 1; i++)
+        {
             char *filepath = argv[i];
 
-            if (access(filepath, F_OK) != 0) {
+            if (access(filepath, F_OK) != 0)
+            {
                 fprintf(stderr, "Ошибка: файл '%s' не существует\n", filepath);
                 continue;
             }
@@ -370,45 +469,43 @@ int main(int argc, char* argv[]){
     {
         char *search_str = argv[argc - 1];
         int found_any = 0;
-        for (int i = 1; i < argc - 2; i++) {
+        for (int i = 1; i < argc - 2; i++)
+        {
             FILE *list_file = fopen(argv[i], "r");
-            if (!list_file) {
+            if (!list_file)
+            {
                 printf("Ошибка открытия файла: %s\n", argv[i]);
-                continue;  // Пропускаем недоступные файлы
+                continue;
             }
-
-            char filename[BUFFER_SIZE];
-
-            while (fgets(filename, sizeof(filename), list_file) != NULL) {
-                trim_whitespace(filename);
-                printf("Захожу в файл с таким названием: %s\n", filename);
-                // Проверяем, не пустая ли строка
-                if (strlen(filename) == 0) continue;
-
-                pid_t pid = fork();
-                if (pid == -1) {
-                    perror("Ошибка fork()");
-                    exit(EXIT_FAILURE);
-                } else if (pid == 0) {
-                    if (find_string_in_file(filename, search_str)) {
-                        printf("Найдено в файле: %s\n", filename);
-                        exit(EXIT_SUCCESS);
-                    }
-                    exit(EXIT_FAILURE);
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                perror("Ошибка fork()");
+                exit(EXIT_FAILURE);
+            }
+            else if (pid == 0)
+            {
+                if (find_string_in_file(argv[i], search_str))
+                {
+                    printf("Найдено в файле: %s\n", argv[i]);
+                    exit(EXIT_SUCCESS);
                 }
+                exit(EXIT_FAILURE);
             }
-
             fclose(list_file);
         }
 
         int status;
-        while (wait(&status) > 0) {
-            if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS) {
+        while (wait(&status) > 0)
+        {
+            if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
+            {
                 found_any = 1;
             }
         }
 
-        if (!found_any) {
+        if (!found_any)
+        {
             printf("Строка не найдена ни в одном из файлов.\n");
         }
     }
